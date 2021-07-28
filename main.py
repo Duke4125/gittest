4082f9a7dce8dbbe66df8e5cfaa29aac5badb339
Duke <64605436+Duke4125@users.noreply.github.com>
2021-07-28 18:32:27 +0100

    added more to main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("This is the git test")
+name = input("What is your name? ")
+age = input(int("What is your age? "))
+
+if age == =< "18":
+    print("You are a child")
+else:
+    print("You're an adult!")
