83f149701e2412f927fe0db5b3f93bc0ca3d76d0
Duke <64605436+Duke4125@users.noreply.github.com>
2021-07-28 18:36:11 +0100

    fixed stuff in main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 name = input("What is your name? ")
-age = input(int("What is your age? "))
+age = int(input("What is your age? "))
 
-if age == =< "18":
+if age <= (int(18)):
     print("You are a child")
 else:
     print("You're an adult!")
